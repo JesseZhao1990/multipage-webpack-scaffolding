6ce7948985e8f80d3ffeddb51d5069b3efd73657
zhaojianxin <215696013@qq.com>
2018-07-09 22:33:08 +0800

    增加资源引用的事例，供参考

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,3 +1,11 @@
 /* eslint-disable */
 import './about.less';
 console.log('11111');
+
+import imgUrl from '../../images/cat.jpeg';
+
+
+const img = document.createElement("img");
+img.style.width = "200px";
+img.src = imgUrl;
+document.body.appendChild(img);
