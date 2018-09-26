bd8ecfe56e39a5eb2ded4171b99aba5536bbbaa9
zhaojianxin <215696013@qq.com>
2018-09-26 16:50:23 +0800

    开发环境下支持css热加载

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -68,24 +68,22 @@ let webpackConfig = {
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader'],
-        }),
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
       },      
       {
         test: /\.(sass|scss)$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-        }),
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
       },
     ]
   },
   plugins:[
-    new ExtractTextPlugin({
-     filename: 'static/css/[name].[hash:7].css'
-    }),
     //设置每一次build之前先删除dist  
     new CleanWebpackPlugin(  
       ['dist/*',],　     //匹配删除的文件  
